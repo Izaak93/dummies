b03f8fc9aac37625b6249d3ce5d63faecfd03746
Artur Laskowski <artur.pi.laskowski@gmail.com>
2017-01-05 01:45:44 +0100

    ADD: Creation of divs in js.

--- dummies.js
+++ dummies.js
@@ -16,3 +16,24 @@ function showMutations() {
     var slider_input = document.getElementById("mutation_percentage");
     slider_value_div.innerHTML = slider_input.value;
 }
+
+function createDiv(div_number, i, j) {
+    var div = document.createElement("div");
+    div.setAttribute("id", div_number * i + j);
+    div.setAttribute("class", "dummy size-" + div_number);
+    div.innerHTML = i*div_number + j;
+    return div;
+}
+
+function createEssential() {
+    var essential = document.getElementById("essential");
+    var div_number = Number(document.getElementById("dummies_number").value);
+    console.log("Divs number: " + div_number);
+
+    essential.innerHTML = "";
+    for(var i = 0; i < div_number; ++i) {
+        for(var j = 0; j < div_number; ++j) {
+            essential.appendChild(createDiv(div_number, i, j));
+        }
+    }
+}
